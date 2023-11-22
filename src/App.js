f93bf0cfacfedacf4e6d7a2daf946d4c17daeaf5
Richard McSharry <rmcsharry@gmail.com>
2023-11-22 10:53:08 +0100

    add axios defaults

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import axios from 'axios';
+
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
+const client = axios.create({
+  baseURL: "http://127.0.0.1:8000",
+});
 
 function App() {
   return (
