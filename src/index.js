a5f1fd00aa70a15b1e2a6a774028441f5158a93d
Richard McSharry <rmcsharry@gmail.com>
2023-11-22 10:53:24 +0100

    add css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
