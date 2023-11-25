d1ed0e7deb243eb2b82f00ebe082dda09491abef
Richard McSharry <rmcsharry@gmail.com>
2023-11-25 20:38:57 +0100

    setup .env for base api url

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -6,7 +6,8 @@ axios.defaults.withCredentials = true;
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 axios.defaults.withXSRFToken = true;
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = process.env.REACT_APP_BASE_API_URL;
+console.log(`API_BASE_URL: ${API_BASE_URL}`);
 
 const axiosClient = axios.create({
   baseURL: API_BASE_URL,
